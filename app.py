8c6c15a5055cdcccce825752a70da54dfcda52e6
SSAKTHITSELVAN <ssakthitselvan7@gmail.com>
2024-12-17 21:47:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+
 import streamlit as st
+import os
+import google.generativeai as genai
 
-# Title
-st.title("Minimal Streamlit App")
+genai.configure(api_key = os.getenv("GOOGLE_API_KEY"))
 
-# Input Section
-st.header("User Input")
-name = st.text_input("What's your name?", "")
+model = genai.GenerativeModel("gemini-pro")
+def get_gemini_response(question):
+    response = model.generate_content(question)
+    return response.text
 
-# Output Section
-if name:
-    st.write(f"Hello, {name}! Welcome to Streamlit!")
-else:
-    st.write("Please enter your name above.")
+
+st.set_page_config(page_title="Q$A Demo")
+
+st.header("Gemini LLM Application")
+
+input = st.text_input("Input : ", key="input")
+submit = st.button("Ask the question")
+
+
+if submit:
+    response = get_gemini_response(input)
+    st.subheader("The response is ")
+    st.write(response)
